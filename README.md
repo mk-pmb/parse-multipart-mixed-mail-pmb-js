0a108cdb4f5dfe9c61aa329048f28923e6921d3f
mk-pmb <mk-pmb@users.noreply.github.com>
2020-09-13 04:08:13 +0200

    v0.1.0

--- README.md
+++ README.md
@@ -14,14 +14,25 @@ body parts.
 API
 ---
 
-This module exports one function:
+This module exports one function, which carries another function:
 
 ### parseMail(raw)
 
 Its API is the same as `splitParseHeaders()` from
 [parse-mail-attachment-pmb][matt],
-except that `body` will be an array of raw body parts.
+except that `body` will be an array of raw body parts as produced by
+`.splitBody()`.
 Use `splitParseHeaders()` or `parseAttachment()` to process them further.
+
+
+
+### .splitBody(raw)
+
+Split just the body parts, and return them.
+Assumes the first line of `raw` is a boundary.
+Whitespace at the end of (some) body parts may have been stripped.
+
+
 
 
 
---
+++ split.mjs
@@ -0,0 +1,30 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import mAtt from 'parse-mail-attachment-pmb';
+import mustBe from 'typechecks-pmb/must-be';
+
+
+const EX = function parseMail(raw) {
+  const mail = mAtt.splitParseHeaders(raw);
+  mustBe('eeq:"multipart/mixed"', 'MIME type')(mail.cType);
+  const boundary = mustBe.nest('MIME boundary in mail headers',
+    mail.ctDetails.boundary);
+  if (!mail.body.startsWith('--' + boundary + '\n')) {
+    throw new Error('The body must start with the MIME boundary.');
+  }
+  mail.body = EX.splitBody(mail.body);
+  return mail;
+};
+
+
+Object.assign(EX, {
+  splitBody(raw) {
+    const body = mAtt.normText(raw);
+    const boundary = mustBe.nest('MIME boundary in first line of body',
+      (/^\-{2}\S+\n/.exec(body) || false)[0]);
+    return body.slice(boundary.length).split('\n' + boundary);
+  },
+});
+
+
+export default EX;
